d01675674e7a6cc3094faf45c7823d903ac8f88a
niksmac <hello@nikhilmohan.in>
2017-12-13 15:22:36 +0530

    Added connections

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  $('.ui.dropdown').dropdown()
+
   var lis = $('.nodeview ul > li')
 
   for (var i = 0; i < lis.length; i += 6) {
@@ -8,4 +10,25 @@ $(document).ready(function () {
   for (var i = 6; i < lis.length; i += 12) {
     lis.slice(i, i + 6).wrapAll("<div class='rightol'></div>")
   }
+
+  $('.skipped')
+    .prev('.reached')
+    .removeClass('reached')
+    .attr('id', 'start')
+
+  $('.skipped')
+    .next('.reached')
+    .attr('id', 'end')
+})
+const connectVar = {
+  paintStyle: { stroke: '#ff9835', strokeWidth: 3 },
+  source: 'start',
+  target: 'end',
+  anchor: ['Left', 'Left'],
+  endpoint: 'Blank',
+  overlays: ['Arrow'],
+  anchor: [0, 1, 0, 1]
+}
+jsPlumb.ready(function() {
+  jsPlumb.connect(connectVar)
 })
